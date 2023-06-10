53350fdc450cb21e4d18955b4616066d6957e54b
Mari <matorina.mariya@mail.ru>
2023-06-10 11:42:41 +0300

    ex6_base

---
+++ Ex6_Base/Program.cs
@@ -0,0 +1,14 @@
+﻿int a = 1;
+int b = 3;
+int c = 4;
+int d = 2;
+int e = 1;
+int max = a;
+
+if (b>max) max = b;
+if (c>max) max = c;
+if (d>max) max = d;
+if (e>max) max = e;
+
+Console.Write("max = ");
+Console.WriteLine(max);
