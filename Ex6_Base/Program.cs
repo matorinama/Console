68a89e5d8368a78342a93530f0a8ec8dce6bfbb4
Mari <matorina.mariya@mail.ru>
2023-06-10 12:17:53 +0300

    ex7_while

--- Ex6_Base/Program.cs
+++ Ex6_Base/Program.cs
@@ -5,10 +5,10 @@ int d = 2;
 int e = 1;
 int max = a;
 
-if (b>max) max = b;
-if (c>max) max = c;
-if (d>max) max = d;
-if (e>max) max = e;
+if (b > max) max = b;
+if (c > max) max = c;
+if (d > max) max = d;
+if (e > max) max = e;
 
 Console.Write("max = ");
 Console.WriteLine(max);
---
+++ Ex7_While/Program.cs
@@ -0,0 +1,42 @@
+﻿Console.Clear();
+//Console.SetCursorPosition(x, y);
+//Console.WriteLine("+");
+
+int xa = 40, ya = 1,
+    xb = 1, yb = 30,
+    xc = 80, yc = 30;
+
+Console.SetCursorPosition(xa, ya);
+Console.Write("+");
+Console.SetCursorPosition(xb, yb);
+Console.Write("+");
+Console.SetCursorPosition(xc, yc);
+Console.Write("+");
+
+int x = xa, y = ya;
+int count = 0;
+
+while (count < 10000)
+{
+    int what = new Random().Next(0, 3);
+    if (what == 0)
+    {
+        x = (x + xa) / 2;
+        y = (y + ya) / 2;
+    }
+    if (what == 1)
+    {
+        x = (x + xb) / 2;
+        y = (y + yb) / 2;
+    }
+    if (what == 2)
+    {
+        x = (x + xc) / 2;
+        y = (y + yc) / 2;
+    }
+    Console.SetCursorPosition(x, y);
+    Console.Write("+");
+    count++;
+}
+
+
